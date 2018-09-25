cceec7d4fea06bdee1723e7cf80dd3a89573316b
hsuchristopher <cristfa.chang@gmail.com>
2018-09-26 02:17:34 +0800

    Initial Template for Homepage

--- html/index.php
+++ html/index.php
@@ -1,3 +1,115 @@
-<!DOCTYPE html>
+<!--
+Author: Christopher Chang
+Date: Tuesday 25th September 2018
+Purpose: CCSEP Assignment 2018, the homepage given when the web application starts
+-->
 
-<html lang="en">
+<!DOCTYPE html>
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>OH MY GOD!!</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" type="text/css" media="screen" href="./css/bootstrap.min.css"/>
+        <link rel="stylesheet" type="text/css" media="screen" href="./css/main.css"/>
+        <!-- My Own CSS -->
+        <link rel="stylesheet" type="text/css" media="screen" href="./css/MYOWNCSS/index.css">
+        <!-- Bootstrap Scripts> -->
+        <script src="./js/jquery-3.3.1.min.js"></script>
+        <script src="./js/popper.min.js"></script>
+        <script src="./js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+    
+        <div class="wrapper">
+            <!-- Sidebar -->
+            <nav id="sidebar">
+                <div class="sidebar-header">
+                    <h3>Funimation Knockoff!</h3>
+                </div>
+
+                <!-- Begin List for Side Bar -->
+                <ul class="list-unstyled components">
+                    <p>Menu Options</p>
+                    <li class="active">
+                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
+                        <ul class="collapse list-unstyled" id="homeSubmenu">
+                            <li>
+                                <a href="#">Home 1</a>
+                            </li>
+                            <li>
+                                <a href="#">Home 2</a>
+                            </li>
+                            <li>
+                                <a href="#">Home 3</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
+                        <ul class="collapse list-unstyled" id="pageSubmenu">
+                            <li>
+                                <a href="#">Page 1</a>
+                            </li>
+                            <li>
+                                <a href="#">Page 2</a>
+                            </li>
+                            <li>
+                                <a href="#">Page 3</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+            
+            <!-- Page Content --> 
+            <div id="content">
+                <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+                    <div class="container-fluid">
+                        <!-- CREATE THE TOGGLE SIDEBAR BUTTON --> 
+                        <button type="button" id="sidebarCollapse" class="btn btn-default btn-lg">
+                            <i class="fas fa-align-left"></i>
+                            <span class="glyphicon glyphicon-nav" aria-hidden="true"></span>
+                        </button>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        <!-- JAVASCRIPT FUNCTIONS -->
+        <!-- Toggle Sidebar -->
+        <script type="text/javascript">
+        $(document).ready(function(){
+            $("#sidebarCollapse").on("click", function(){
+                $('#sidebar').toggleClass('active');
+            });
+        });
+        </script>
+                
+
+
+
+
+
+
+    </body>
+
+
+
+               
+
+</html>
